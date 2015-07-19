94c66113988e3387402a5ebde59f8efcccd31325
ajayrfhp <ajayrfhp1710@gmail.com>
2015-07-19 16:41:33 +0530

    added closestFeature Finder

--- blogGroupper/__main__.py
+++ blogGroupper/__main__.py
@@ -2,7 +2,22 @@ import feedparser
 import re
 from  parser import *
 import json
+from functions import *
+from math import sqrt
 
+'''
+REFERENCE : http://www.econ.upf.edu/~michael/stanford/maeb7.pdf
+
+INTUITION : 
+	Generate simillarity matrix for all features 
+	Each feature is a  node .
+	choose 2 closest features ,create their parent. 
+	The feature vector of parent  is combined node is the mean of the 2 contributing nodes.
+	Repeat untill u get all nodes are connected and u get a single root.
+
+
+
+'''
 
 
 
@@ -10,26 +25,25 @@ import json
 
 data = {}
 
-def generateFeatures():
-	with open("blogGroupper/feedList.txt","r") as f:	
-		urls = f.readlines()
-		for url in urls:
-			try :
-				title,featureVector =  generateFeed(url)
-				data[title] = featureVector
-				
-			except AttributeError:
-				continue
-			
-			print urls.index(url)
-			
-	with open ('blogGroupper/feature.json','wb') as f2:
-		json.dump(json.dumps(data),f2)
 
 
-
-with open('blogGroupper/feature.json') as f:
+with open('feature.json') as f:
      data = json.loads(json.load(f))
 
 
-print data.keys()
+
+
+
+data = [[0,0.5000,0.4286,1.0000,0.2500,0.6250,0.3750],
+ [0.5000,0,0.7143,0.8333,0.6667,0.2000,0.7778],
+ [0.4286,0.7143,0,1.0000,0.4286,0.6667,0.3333],
+ [1.0000,0.8333,1.0000,0,1.0000,0.8000,0.8571],
+ [0.2500,0.6667,0.4286,1.0000,0,0.7778,0.3750],
+ [0.6250,0.2000,0.6667,0.8000,0.7778,0,0.7500],
+ [0.3750,0.7778,0.3333,0.8571,0.3750,0.7500,0]]
+
+
+ 
+ 
+
+ data = numpy.array(data)
--- blogGroupper/functions.py
+++ blogGroupper/functions.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def pearson(v1,v2):
 	sum1 = sum(v1)
 	sum2 = sum(v2)
@@ -12,5 +14,31 @@ def pearson(v1,v2):
 	den = sqrt((sum1Squares - (sum1*sum1)/len(v1)) * (sum2Squares - (sum2*sum2)/len(v2)) )
 	if(den == 0):
 		return 0
-	pearson =  1 - (num / den)
+	pearson =   (num / den)
 	return pearson
+
+
+def generateFeatures():
+	with open("blogGroupper/feedList.txt","r") as f:	
+		urls = f.readlines()
+		for url in urls:
+			try :
+				title,featureVector =  generateFeed(url)
+				data[title] = featureVector
+				
+			except AttributeError:
+				continue
+			
+			print urls.index(url)
+			
+	with open ('blogGroupper/feature.json','wb') as f2:
+		json.dump(json.dumps(data),f2)
+
+
+def getClosestFeatures(data):
+	data = np.tril(data)
+	maxValue = np.tril(data).max()
+	indices = [((i,j) if data[i][j]==maxValue else (-1,-1) ) for j in range(data.shape[1])    for i in range(data.shape[0])  ]
+	indices = filter(lambda a:a!=(-1,-1),indices)
+ 	index = indices[0]
+ 	return index
