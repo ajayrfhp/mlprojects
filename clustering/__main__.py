92802f1039c33ccef2c5065ef93d65f1ee6c5dab
ajayrfhp <ajayrfhp1710@gmail.com>
2015-07-27 21:04:17 +0530

    done clustering

--- clustering/__main__.py
+++ clustering/__main__.py
@@ -1,18 +1,26 @@
 import matplotlib.pyplot as p
 import numpy 
 from numpy import *
-from pylab import *
 from kmeans import *
 data = [[1,2],[2,2],[3,2],[2,3],[4,2]]
-plot(data,'.')
+
 
 data = numpy.array(data)
 
+centers = numpy.array(buildModel(data,2))
 
 
-buildModel(data,2)
 
-#p.show()
+	
+
+p.plot(data[:,0],data[:,1],'.')
+p.plot(centers[:,0],centers[:,1],'bo')		
+p.axis([0, 5, 0,5])
+p.show()
+
+
+
+
 
 
 
--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,5 +1,7 @@
 import numpy 
 from numpy import *
+import time 
+import matplotlib.pyplot as p
 
 def euclideanDistance(p1,p2):
 	distance = 0
@@ -14,31 +16,87 @@ def buildModel(data,k):
 	for i in range(len(data)):
 		points[i] = data[i]
 
-	centres = data[0:k]
-	pointCenters = {}
-	newCenters = []
-	for i in points.keys():
-		minDistance = 999999999999999999
-		thisCenter = -1
-		for center in centres:
-			thisDistance = euclideanDistance(points[i],center)
-			
-			if(thisDistance < minDistance):
-				minDistance = thisDistance
-				thisCenter = center
-				#print "hi"
-			#print str(thisDistance) + str(thisCenter) 
-					
-		pointCenters[i] = thisCenter
-	
-		newCenters.append(tuple(thisCenter))
-	
-	print newCenters		
 
-	newCenters = list(set(newCenters))	
 	
-	print newCenters
-	print centres
+	centres = data[0:k]
+
+	while(1):
+			# START WITH RANDOM CENTRES,COMPUTE SHORTEST CENTRE FOR EACH DATA POINT
+
+		
+		pointCenters = {}
+		newCenters = []
+		for i in points.keys():
+			minDistance = 999999999999999999
+			thisCenter = -1
+			for center in centres:
+				thisDistance = euclideanDistance(points[i],center)
+				
+				if(thisDistance < minDistance):
+					minDistance = thisDistance
+					thisCenter = center
+					#print "hi"
+				#print str(thisDistance) + str(thisCenter) 
+						
+			pointCenters[i] = thisCenter
+		
+			newCenters.append(tuple(thisCenter))
+		
+
+		newCenters = list(set(newCenters))	
+		
+		# point Centers contain the shortest centre from each point
+		newNewCenter = []
+		for center in newCenters:
+			#print "-----------------------------"
+			#print center
+			thisCenter = []
+			for i in range(len(center)):
+				thisCenter.append(0)
+
+			cnt = 0
+			for key in pointCenters.keys():
+				flag = True
+				for dimension in range(len(center)):
+					if(pointCenters[key][dimension] != center[dimension]):
+						flag = False
+					
+						
+				if(flag):
+					#print points[key]
+					cnt +=1
+					for dimension in range(points[key].shape[0]):
+						thisCenter[dimension] += points[key][dimension]
+
+			for dimension in range((points[key].shape[0])):
+						thisCenter[dimension] =  float(thisCenter[dimension])/cnt
+				
+				
+			newNewCenter.append(thisCenter)
+
+		#print centres	
+		flag = True
+		for i in range(len(newCenters[0])):
+			for j in range(len(newCenters[1])):
+				if(newNewCenter[i][j] != centres[i][j]):
+					flag = False
+		if(flag == True):
+			break			
+		centres = newNewCenter	
+		p.clf()
+		centres = numpy.array(centres)	
+		p.plot(data[:,0],data[:,1],'.')
+		p.plot(centres[:,0],centres[:,1],'bo')		
+		p.axis([0, 5, 0,5])
+		p.show()		
+		time.sleep(0.5)
+	
+
+	return centres
+
+
+
+
 	
 	## determined nearest centeres 
 
